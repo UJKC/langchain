a725290016262a87939ad902ab8e5122453a77b9
Ujwal KC <ujwalkcsps@gmail.com>
2023-12-24 22:49:41 +0530

    ok now

--- section2/first.py
+++ section2/first.py
@@ -24,7 +24,7 @@ Given the information here: {information}, I want you to create:
 
 summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-llm = ChatOpenAI(temperature= 0, model_name="gpt-3.5-turbo", openai_api_key="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")
+llm = ChatOpenAI(temperature= 0, model_name="gpt-3.5-turbo", openai_api_key="")
 
 chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
